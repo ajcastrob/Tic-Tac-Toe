c2d5f6d77cdb45e3d7af750af457aaa561bc8091
José Castro <a.j.castro.b@gmail.com>
2026-05-04 19:31:40 -0500

    Add
    Creando el tablero y los turnos del juego.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,53 @@
+import { useState } from "react";
 import "./App.css";
 
+const TURNS = {
+  X: "x",
+  O: "o",
+};
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = isSelected ? "is-selected" : "";
+
+  const handleClick = () => {
+    updateBoard();
+  };
+
+  return (
+    <div onClick={handleClick} className={`square ${className}`}>
+      {children}
+    </div>
+  );
+};
+
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
+
+  const updateBoard = () => {
+    //Establecer el nuevo turno.
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+    //Establecer el nuevo tablero.
+    const newBoard = [...board];
+  };
+
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {board.map((square, index) => {
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {square}
+            </Square>
+          );
+        })}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
     </main>
   );
 }
